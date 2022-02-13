7b76cc6a406e52faa5d9229a7fa3c7b64b45b71d
ekazfarm <syachferdian4@gmail.com>
2022-02-13 20:48:33 +0700

    renew

--- ntp-config/ntp.sh
+++ ntp-config/ntp.sh
@@ -8,10 +8,18 @@ systemctl start chronyd
 
 systemctl enable chronyd
 
+timedatectl | grep "NTP synchronized"
+
+timedatectl set-ntp yes
+
 systemctl restart chronyd
 
 systemctl status chronyd
 
+chronyc tracking
+
 chronyc sources
+
+echo "mohon pastikan dengan command date apakah masih sama karena akan mempengaruhi server saat menjalankan call center autodial untuk pengguna Issabel"
 
 echo "this script created by eka because he has not have activities or gabut"
